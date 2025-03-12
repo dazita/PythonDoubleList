1aafd0193308dd18815bec4316f96f451d0d4253
Pulido098 <oscar.pulido03@uptc.edu.co>
2025-03-12 15:48:08 -0500

    Getters and setters necesarios

--- DoubleList.py
+++ DoubleList.py
@@ -2,83 +2,99 @@ from DoubleNode import DoubleNode
 
 class DoubleLinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.__head = None
+        self.__tail = None
+
+    # Getter para head
+    def get_head(self):
+        return self.__head
+
+    # Setter para head
+    def set_head(self, node):
+        self.__head = node
+
+    # Getter para tail
+    def get_tail(self):
+        return self.__tail
+
+    # Setter para tail
+    def set_tail(self, node):
+        self.__tail = node
 
     def insert(self, data):
         aux = DoubleNode(data)
         if self.is_empty():
-            self.head = aux
-            self.tail = aux
+            self.__head = aux
+            self.__tail = aux
         else:
-            self.tail.next = aux
-            aux.prev = self.tail
-            self.tail = aux
+            self.__tail.set_next(aux)
+            aux.set_prev(self.__tail)
+            self.__tail = aux
 
     def is_empty(self):
-        return self.head is None
+        return self.__head is None
 
     def exist(self, data):
-        aux = self.head
+        aux = self.__head
         while aux is not None:
-            if aux.data == data:
+            if aux.get_data() == data:
                 return True
-            aux = aux.next
+            aux = aux.get_next()
         return False
 
     def remove(self, data):
-        aux = self.head
+        aux = self.__head
         while aux is not None:
-            if aux.data == data:
-                if self.head.data == data:
-                    if self.head == self.tail:
-                        self.head = None
-                        self.tail = None
+            if aux.get_data() == data:
+                if self.__head.get_data() == data:
+                    if self.__head == self.__tail:
+                        self.__head = None
+                        self.__tail = None
                     else:
-                        self.head = self.head.next
-                        self.head.prev = None
-                elif self.tail.data == data:
-                    self.tail = self.tail.prev
-                    self.tail.next = None
+                        self.__head = self.__head.get_next()
+                        self.__head.set_prev(None)
+                elif self.__tail.get_data() == data:
+                    self.__tail = self.__tail.get_prev()
+                    self.__tail.set_next(None)
                 else:
-                    aux.next.prev = aux.prev
-                    aux.prev.next = aux.next
+                    aux.get_next().set_prev(aux.get_prev())
+                    aux.get_prev().set_next(aux.get_next())
                 return True
-            aux = aux.next
+            aux = aux.get_next()
         return False
 
     def size(self):
         counter = 0
-        aux = self.head
+        aux = self.__head
         while aux is not None:
-            aux = aux.next
+            aux = aux.get_next()
             counter += 1
         return counter
 
     def get(self, index):
-        aux = self.head
+        aux = self.__head
         counter = 0
         while aux is not None and counter < index:
-            aux = aux.next
+            aux = aux.get_next()
             counter += 1
         if aux is not None:
-            return aux.data
+            return aux.get_data()
         raise IndexError("Index out of range")
 
     def show(self):
         result = []
-        aux = self.head
+        aux = self.__head
         while aux is not None:
-            result.append(str(aux.data))
-            aux = aux.next
+            result.append(str(aux.get_data()))
+            aux = aux.get_next()
         return "\n".join(result)
 
     def show_reversed(self):
         result = []
-        aux = self.tail
+        aux = self.__tail
         while aux is not None:
-            result.append(str(aux.data))
-            aux = aux.prev
+            result.append(str(aux.get_data()))
+            aux = aux.get_prev()
         return "\n".join(result)
 
     def __iter__(self):
@@ -87,11 +103,11 @@ class DoubleLinkedList:
 
     def __next__(self):
         if self._current_node is None:
-            if self.head is None:
+            if self.__head is None:
                 raise StopIteration
-            self._current_node = self.head
+            self._current_node = self.__head
         else:
-            self._current_node = self._current_node.next
+            self._current_node = self._current_node.get_next()
             if self._current_node is None:
                 raise StopIteration
-        return self._current_node.data
+        return self._current_node.get_data()
--- DoubleNode.py
+++ DoubleNode.py
@@ -1,5 +1,25 @@
 class DoubleNode:
     def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
+        self.__data = data
+        self.__next = None
+        self.__prev = None
+
+    
+    def get_data(self):
+        return self.__data
+
+    def get_next(self):
+        return self.__next
+
+    def get_prev(self):
+        return self.__prev
+
+
+    def set_data(self, data):
+        self.__data = data
+
+    def set_next(self, next_node):
+        self.__next = next_node
+
+    def set_prev(self, prev_node):
+        self.__prev = prev_node
